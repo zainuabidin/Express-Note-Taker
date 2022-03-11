143fafaff647f673a58221fd3297e7bfac56520f
Zain Abidin <xan.abdn@gmail.com>
2022-03-11 12:09:30 +0000

    api post route working

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,6 +1,8 @@
+const { reverse } = require("dns")
 const express = require("express")
 const router = express.Router()
 const fs =  require("fs")
+const { resourceLimits } = require("worker_threads")
 
 router.get("/api/notes",(req,res)=>{
     fs.readFile("db/db.json","utf-8",(err,data)=>{
@@ -11,6 +13,28 @@ router.get("/api/notes",(req,res)=>{
             }
     })
 })
+
+router.post("/api/notes",(req,res)=>{
+    fs.readFile("db/db.json","utf-8",(err,data)=>{
+        if(err){
+            console.log(err)
+        }
+        else{
+            const db = JSON.parse(data)
+            const newNote=req.body
+            db.push(newNote)
+            fs.writeFile("db/db.json",JSON.stringify(db),(err)=>{
+                if(err){
+                    console.log(err)
+                }
+                else{
+                    return res.json(db)
+                }
+            })
+        }
+    })
+})
+
 
 
 module.exports = router;
